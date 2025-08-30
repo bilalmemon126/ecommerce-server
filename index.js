7e9fd3abeed99f11ea757319f566966e3e30b059
bilalmemon126 <bilalm3m9n@gmail.com>
2025-08-30 17:00:25 +0500

    index files changes

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ const port = 3000;
 app.use(express.json());
 app.use(cookieParser())
 
+app.get("/", (req,res) => {
+  res.send("hello world")
+})
 
 app.use(registrationRoutes)
 app.use(emailVerificationRoutes)
